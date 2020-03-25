3500f7ae172964daac3be5675c5d91c229141781
Viktor Hollanders <viktorhollanders@gmail.com>
2020-03-25 15:49:05 +0000

    add viewport data

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ function Home() {
     <title>BioBorgari</title>
     <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta
      key="description"
      name="description"
